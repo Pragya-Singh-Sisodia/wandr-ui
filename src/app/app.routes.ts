bd566f70d4d8541282d64603e32e2df4511010cf
Pragya-Singh-Sisodia <pragyasingh3925@gmail.com>
2026-07-21 00:05:36 +0530

    Build shared application header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,42 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { MainLayout } from './layouts/main-layout/main-layout';
+
+import { Home } from './features/home/pages/home/home';
+import { Planner } from './features/planner/pages/planner/planner';
+import { Weather } from './features/weather/pages/weather/weather';
+import { Assistant } from './features/assistant/pages/assistant/assistant';
+import { Trips } from './features/trips/pages/trips/trips';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainLayout,
+    children: [
+      {
+        path: '',
+        component: Home
+      },
+      {
+        path: 'planner',
+        component: Planner
+      },
+      {
+        path: 'weather',
+        component: Weather
+      },
+      {
+        path: 'assistant',
+        component: Assistant
+      },
+      {
+        path: 'trips',
+        component: Trips
+      }
+    ]
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
